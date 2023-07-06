1fc4b1bff70e4ffc906bafd14bccb742e5268c4c
Manish Tomar <manish.tomar@moondive.co>
2023-07-06 10:41:51 +0530

    first commit

---
+++ src/Button.js
@@ -0,0 +1,9 @@
+import React from "react";
+import './ReuseButton.css';
+
+const Button = () =>{
+    return (
+        <button className="button-reuse">Reuse me!!</button>
+    )
+}
+export default Button;
